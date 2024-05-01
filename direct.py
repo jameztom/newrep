9b954158b727ba4c9fd2aefe1bc4d25bf3a1a562
jameztom <154118881+jameztom@users.noreply.github.com>
2024-05-02 01:19:58 +0530

    Create direct.py

---
+++ direct.py
@@ -0,0 +1,15 @@
+git clone to clone the repository
+
+Adding an Upstream Remote
+original repository that you forked is known as the "upstream
+git remote -v
+git remote add upstream <upstream-url>
+
+
+Keeping Your Forked Repository Up to Date: `git checkout main`
+git merge upstream/main`
+`git push origin main`
+
+Creating Branches for Collaborative Work
+gitcheckout -b <branch-name>`
+
