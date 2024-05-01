0450c5cc3ff9f4b2fc23c3af089970aa1757458c
jameztom <154118881+jameztom@users.noreply.github.com>
2024-05-02 01:16:03 +0530

    Create create.py

---
+++ create.py
@@ -0,0 +1,5 @@
+"Issues" - "New issue" - 	Fill in the issue title and description. -
+Within the issue, use the "Assignees" section to assign 
+Apply labels to categorize and prioritize 
+git commit -m "Fix #1: Resolve issue with feature X" Replace "1" with the actual issue number.
+iv.	git push origin main
