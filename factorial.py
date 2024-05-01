9f16557bd68fe7d1eaa77c7c2ee660b23ee8be81
jameztom <154118881+jameztom@users.noreply.github.com>
2024-05-02 01:22:35 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,35 @@
+•	Create a new repository: git init
+•	Clone a repository: git clone [respository URL]
+•	Add a remote repository: git remote add [name] [repository URL]
+•	Fetch changes from a remote repository: git fetch
+•	Pull changes from a remote repository: git pull
+•	Push changes to a remote repository: git push 
+
+Use branching and merging
+•	Create a new branch: git branch [name]
+•	Switch to a branch: git checkout [name]
+•	Merge a branch: git merge [name]
+
+Use Pull requests
+•	Create a new pull request: git request-pull [branch] [repository URL]
+•	Review and merge a pull request: git pull-request
+•	Close a pull request: git request-pull -C [branch] [repository URL] 
+
+Use Issue Tracking
+•	Create a new issue: git commit -m “[issue number] [commit message]”
+•	Assign an issue to a team member: git assign [username]
+•	Clone an issue: git clone [issue number]
+•	Reopen an issue: git reopen [issue number] 
+
+Use Git submodules
+Add a Git submodule: git submodule add [repository URL]
+Update a Git submodule: gitsubmodule update
+Remove a Git submodule: git submodule deinit [submodule path]
+
+Use Git aliases
+Set up a Git alias: git config –global alias.[alias name] ‘[Git command]’
+
+Use a Git alias: git [alias name]
+conflicts: git merge tool
+Delete a branch: git branch -d [name]
+
